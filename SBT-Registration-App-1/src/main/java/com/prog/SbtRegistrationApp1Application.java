694420e392024da2ccdd1c2ade577aafa689a352
MMK <MMK@DESKTOP-KLBI9BQ>
2022-08-19 12:45:47 +0530

    Registration Form Boot

---
+++ SBT-Registration-App-1/src/main/java/com/prog/SbtRegistrationApp1Application.java
@@ -0,0 +1,13 @@
+package com.prog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbtRegistrationApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbtRegistrationApp1Application.class, args);
+	}
+
+}
